b24539a2af6a06d7f09d3a920c5be3f56c9967b5
own-destiny <167978547+own-destiny@users.noreply.github.com>
2024-06-15 17:16:21 +0530

    Update hellowExternal.groovy

--- vars/hellowExternal.groovy
+++ vars/hellowExternal.groovy
@@ -1,5 +1,5 @@
 def call (){
-  def testScript = libraryResources 'scripts/linux/test-script.sh'
+  def testScript = libraryResource 'scripts/linux/test-script.sh'
   sh "echo $testScript"
   // sh './test-script.sh manitheja saturday'
 }
