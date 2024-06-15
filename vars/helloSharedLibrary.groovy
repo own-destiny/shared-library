a8e447478ba1a7d37e12ed65cd1e23a22f77b59e
own-destiny <167978547+own-destiny@users.noreply.github.com>
2024-06-15 16:02:43 +0530

    Update helloSharedLibrary.groovy

--- vars/helloSharedLibrary.groovy
+++ vars/helloSharedLibrary.groovy
@@ -3,7 +3,6 @@ def call() {
   sh "echo Hello YMT, welcome to the shared library learning path. Today is Saturday"
 }
 
-================
 // example pipeline
 // ====================
 // @Library("Builders") _
