1d2b2dac96951d1bf4ecee5b35f379c1add48dec
own-destiny <167978547+own-destiny@users.noreply.github.com>
2024-06-15 17:17:21 +0530

    Update test-script.sh

--- resources/scripts/linux/test-script.sh
+++ resources/scripts/linux/test-script.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-echo "hello $1, The future is AI and Today is $2. Don't procatinate your work"
+# echo "hello $1, The future is AI and Today is $2. Don't procatinate your work"
+echo "hello Manitheja, The future is AI and Today is Saturday. Don't procatinate your work"
