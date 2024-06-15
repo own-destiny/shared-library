222f8c04a98e2801321bfaeab583c6453ccdcf91
own-destiny <167978547+own-destiny@users.noreply.github.com>
2024-06-15 17:30:42 +0530

    Update hellowExternal.groovy

--- vars/hellowExternal.groovy
+++ vars/hellowExternal.groovy
@@ -1,5 +1,14 @@
-def call (String name, String dayOfTheWeek){
-  def testScript = libraryResource 'scripts/linux/test-script.sh'
-  sh "echo $testScript"
-  // sh './test-script.sh manitheja saturday'
+// def call (String name, String dayOfTheWeek){
+//   def testScript = libraryResource 'scripts/linux/test-script.sh'
+//   sh "echo $testScript"
+//   // sh './test-script.sh manitheja saturday'
+// }
+
+// vars/helloExternal.groovy
+def call(String name, String dayOfTheWeek) {
+    def testScript = libraryResource 'scripts/linux/test-script.sh'
+    
+    // Copy the script to the workspace and execute it
+    sh "cp ${testScript} ${workspace}/test-script.sh && chmod +x ${workspace}/test-script.sh"
+    sh "${workspace}/test-script.sh"
 }
