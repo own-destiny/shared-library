3bf2864ede3c9f1b3d0a046439f8ebe5e14c25c7
own-destiny <167978547+own-destiny@users.noreply.github.com>
2024-06-15 18:05:54 +0530

    Update loadScript.groovy

--- vars/loadScript.groovy
+++ vars/loadScript.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]){
-  def testScript = libraryResource 'scripts/linux/test-script.sh'
+  def testScript = libraryResource "scripts/linux/test-script.sh"
   writeFile file: "${config.name}", text: testScript
   sh "chmod +x ./${config.name}"
 }
