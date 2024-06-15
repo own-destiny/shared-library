b2752d82358d62456d89b0c72a89de59358f640f
own-destiny <167978547+own-destiny@users.noreply.github.com>
2024-06-15 17:50:43 +0530

    Update hellowExternal.groovy

--- vars/hellowExternal.groovy
+++ vars/hellowExternal.groovy
@@ -1,8 +1,14 @@
-def call (Map config = [:]){
-  loadScript(name: 'test-script.sh')
-  sh './test-script.sh manitheja saturday'
-}
+// def call (Map config = [:]){
+//   loadScript(name: 'test-script.sh')
+//   sh './test-script.sh manitheja saturday'
+// }
 
+def call(){
+  def testScript = libraryResource 'scripts/linux/test-script.sh'
+  writeFile file: "test-script.sh", text: testScript
+  sh "chmod +x ./test-script.sh"
+  sh "./test-script.sh"
+}
 // vars/helloExternal.groovy 
 // def call(String name, String dayOfTheWeek) {
 //     def testScript = libraryResource 'scripts/linux/test-script.sh'
