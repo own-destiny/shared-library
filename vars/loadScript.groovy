a7c5b67523138bbbe81b6b24e53b151b91177d0e
own-destiny <167978547+own-destiny@users.noreply.github.com>
2024-06-15 17:48:06 +0530

    Create loadScript.groovy

---
+++ vars/loadScript.groovy
@@ -0,0 +1,5 @@
+def call(Map config = [:]){
+  def testScript = libraryResource 'scripts/linux/test-script.sh'
+  writeFile file: "${config.name}", text: testScript
+  sh "chmod +x ./${config.name}"
+}
