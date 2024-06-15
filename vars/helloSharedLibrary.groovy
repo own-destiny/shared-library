30daa7d38a15971dc6932ea81c480a37d15bf74c
own-destiny <167978547+own-destiny@users.noreply.github.com>
2024-06-15 15:58:09 +0530

    Update helloSharedLibrary.groovy

--- vars/helloSharedLibrary.groovy
+++ vars/helloSharedLibrary.groovy
@@ -38,8 +38,8 @@
 // }
 // =================================
 // pass the values dynamically by using Map keyword and more robust excluding positional parameters.
-
 def call(Map config = [:]) {
+  // its just a map and positions do not matter at this point
   sh "echo Hello ${config.name}, welcome to the shared library learning path. Today is ${config.dayOfTheWeek}."
 }
 
