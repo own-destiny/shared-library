de38bfb7eeecc5d6977c88a91d5376b4fcf87357
own-destiny <167978547+own-destiny@users.noreply.github.com>
2024-06-15 17:41:26 +0530

    Update hellowExternal.groovy

--- vars/hellowExternal.groovy
+++ vars/hellowExternal.groovy
@@ -1,14 +1,18 @@
-// def call (String name, String dayOfTheWeek){
-//   def testScript = libraryResource 'scripts/linux/test-script.sh'
-//   sh "echo $testScript"
-//   // sh './test-script.sh manitheja saturday'
-// }
-
-// vars/helloExternal.groovy
-def call(String name, String dayOfTheWeek) {
-    def testScript = libraryResource 'scripts/linux/test-script.sh'
-    
-    // Copy the script to the workspace and execute it
-    sh "cp ${testScript} ${workspace}/test-script.sh && chmod +x ${workspace}/test-script.sh"
-    sh "${workspace}/test-script.sh"
+def call (Map config = [:]){
+  def testScript = libraryResource 'scripts/linux/test-script.sh'
+  writeFile file: "${config.name}", text: testScript
+  sh "chmod +x ./${config.name}"
+  sh "cat ./$(config.name)"
+  // sh "echo $testScript"
+  // sh './test-script.sh manitheja saturday'
 }
+
+// vars/helloExternal.groovy 
+// def call(String name, String dayOfTheWeek) {
+//     def testScript = libraryResource 'scripts/linux/test-script.sh'
+    
+//     // Copy the script to the workspace and execute it
+//     sh "cp ${testScript} ${workspace}/test-script.sh && chmod +x ${workspace}/test-script.sh"
+//     sh "${workspace}/test-script.sh"
+// }
+// got error cp missing
