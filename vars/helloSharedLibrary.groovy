fd2ef88d601d78c4fd0285a5fdc2cf12655c9513
own-destiny <167978547+own-destiny@users.noreply.github.com>
2024-06-15 15:56:04 +0530

    Update helloSharedLibrary.groovy

--- vars/helloSharedLibrary.groovy
+++ vars/helloSharedLibrary.groovy
@@ -1,3 +1,45 @@
-def call(String name, String dayOfTheWeek) {
-  sh "echo Hello ${name}, welcome to the shared library learning path. Today is ${dayOfTheWeek}"
+// def call() {
+//   sh "echo Hello YMT, welcome to the shared library learning path. Today is Saturday"
+// }
+// ================
+// example pipeline:2
+// ====================
+// @Library("Builders") _
+// pipeline {
+//     agent any
+
+//     stages {
+//         stage('Hello') {
+//             steps {
+//                 helloSharedLibrary("Yamavaram Manitheja", "Saturday")
+//             }
+//         }
+//     }
+// }
+// =====================================
+//Example:2 below examples allow us to pass the values dynamically. Excluding positionsl parameter.
+// def call(String name, String dayOfTheWeek) {
+//   sh "echo Hello ${name}, welcome to the shared library learning path. Today is ${dayOfTheWeek}"
+// }
+// ================
+// example pipeline:2
+// ====================
+// @Library("Builders") _
+// pipeline {
+//     agent any
+
+//     stages {
+//         stage('Hello') {
+//             steps {
+//                 helloSharedLibrary("Yamavaram Manitheja", "Saturday")
+//             }
+//         }
+//     }
+// }
+// =================================
+// pass the values dynamically by using Map keyword and more robust excluding positional parameters.
+
+def call(Map config = [:]) {
+  sh "echo Hello ${config.name}, welcome to the shared library learning path. Today is ${config.dayOfTheWeek}."
 }
+
