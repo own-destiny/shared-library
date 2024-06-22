fe150111cd2699a1bb0ca9fc2bc3999500688422
own-destiny <167978547+own-destiny@users.noreply.github.com>
2024-06-22 19:33:36 +0530

    Update helloSharedLibrary.groovy

--- vars/helloSharedLibrary.groovy
+++ vars/helloSharedLibrary.groovy
@@ -1,7 +1,15 @@
 
-def call() {
+def call(testgroovy) {
   sh "echo Hello YMT, welcome to the shared library learning path. Today is Saturday"
   println("Hello from println")
+}
+
+def info(message){
+  echo "INFO: $message"
+}
+
+def message(info){
+  echo "MESSAGE: $INFO"
 }
 
 // example pipeline
