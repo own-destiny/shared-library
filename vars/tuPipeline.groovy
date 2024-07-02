c48c33326635d0b3721049274cd82219d799c5ef
own-destiny <167978547+own-destiny@users.noreply.github.com>
2024-07-02 18:34:14 +0530

    Update tuPipeline.groovy

--- vars/tuPipeline.groovy
+++ vars/tuPipeline.groovy
@@ -7,8 +7,7 @@ def call(){
   }
   switch (pipelineConfig.pipelineType){
     case 'maven-1':
-        // mavenPipeline pipelineConfig
-        println "running maven pipeline groovy"
+        mavenPipeline pipelineConfig
         break
     default:
         echo "Must define a pipelineType in Jenkins.yml (maven, gradle, nodejs, python, tarball, iac-cfn<-appdev>, iac-tf-v<1,2>, iac-tf-mod-publish, iac-nix-publish, avm(-v2), cldnet, helm, kongapi, kongplugin, kongmanifest, dotNetCore, dotNetFramework)!"
