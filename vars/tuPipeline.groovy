5dba387aa53460622e0d79a64ff3d3dc44f46ee7
own-destiny <167978547+own-destiny@users.noreply.github.com>
2024-07-02 16:06:05 +0530

    Update tuPipeline.groovy

--- vars/tuPipeline.groovy
+++ vars/tuPipeline.groovy
@@ -1,11 +1,8 @@
 def call(){
- def pipelineConfig
- pipelineConfig = readYaml file: './pipelineConfig.yml'
- println "$pipelineConfig"
-  // node{
-  //   checkout scm
-  //   pipelineConfig = readYaml file: './pipelineConfig.yml'
-  //   println "$pipelineConfig"
-  //   cleanWs()
-  // }
+  node{
+    checkout scm
+    pipelineConfig = readYaml file: './pipelineConfig.yml'
+    println "$pipelineConfig"
+    cleanWs()
+  }
 }
