ef4443a05c3b405d6308d417591a92bdaf66ca3a
own-destiny <167978547+own-destiny@users.noreply.github.com>
2024-07-02 18:25:20 +0530

    Update tuPipeline.groovy

--- vars/tuPipeline.groovy
+++ vars/tuPipeline.groovy
@@ -2,7 +2,7 @@ def call(){
   node{
     checkout scm
     pipelineConfig = readYaml file: './pipelineConfig.yml'
-    println "$pipelineConfig \n"
+    println "$pipelineConfig +'\n'"
     cleanWs()
   }
 }
