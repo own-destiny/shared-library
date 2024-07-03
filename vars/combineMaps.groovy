fa1c16ac53c210ff5b287f5263f710c7e5e82f3c
own-destiny <167978547+own-destiny@users.noreply.github.com>
2024-07-03 15:56:40 +0530

    Update combineMaps.groovy

--- vars/combineMaps.groovy
+++ vars/combineMaps.groovy
@@ -1,4 +1,14 @@
 def call(Map... maps){
+  Map result
   // Map... maps
   println "maps length: ${maps.length}"
+  if (maps.length == 0){
+    result = [:]
+  }
+  else if (maps.length == 1){
+    result = maps[0]
+  }else{
+    result = [:]
+    maps.each{ map -> println map}
+  }
 }
