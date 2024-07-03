ce6cfbe93274d01f3342f6e137562f1fbbebe9e0
own-destiny <167978547+own-destiny@users.noreply.github.com>
2024-07-03 15:15:49 +0530

    Update mavenPipeline.groovy

--- vars/mavenPipeline.groovy
+++ vars/mavenPipeline.groovy
@@ -2,11 +2,11 @@ def call(Map pipelineConfig){
   // println "This line prints from mavenPipeline.groovy file ${pipelineConfig.pipelineType}"
   println "reading the default config values from the globaldefault and maven default values"
   def globalConfigFile = libraryResource 'maven/globalDefault.yml'
-  def globalConfig = readYaml file: globalConfigFile
+  def globalConfig = readYaml text: globalConfigFile
   def defaultConfigFile = libraryResource 'maven/mavenDefault.yml'
-  def defaultConfig = readYaml file: defaultConfigFile
+  def defaultConfig = readYaml text: defaultConfigFile
   def protectedConfigFile = libraryResource 'maven/mavenProtected.yml'
-  def protectedConfig = readYaml file: protectedConfigFile
+  def protectedConfig = readYaml text: protectedConfigFile
   println "calling combine maps functions"
   def config = combineMaps (globalConfig, defaultConfig, pipelineConfig, protectedConfig)
 }
